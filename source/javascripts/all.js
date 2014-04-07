7fd0272240fa8d8ace61b1a068bca4de03805a73
Adam Butler <adam@littlebigmedia.co.uk>
2014-04-07 23:01:48 +0100

    all.js no longer requires the whole tree

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,17 @@
+//= require './jquery'
 //= require './jquery_ui'
-//= require_tree .
+//= require './angular'
+//= require './peer'
+//= require './device'
+
+//= require './jquery.tocify'
+//= require './lang_selector'
+//= require './slides'
+
+//= require './webrtc'
+
+//= require_tree ./angular/modules
+//= require_tree ./angular/directives
+//= require_tree ./angular/filters
+//= require_tree ./angular/factories
+//= require_tree ./angular/controllers
